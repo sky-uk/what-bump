a9a96247ae72e6ef909358a2a5cf772a8e3fd30a
colomboalb <alberto.colombo@skytv.it>
2020-01-20 13:25:18 +0100

    build(release): improve script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 set -e
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git tag TEMP-RELEASE HEAD
 git checkout master
-git merge TEMP-RELEASE
+git merge TEMP-RELEASE -m "Merge $CURRENT_BRANCH into master"
 LATEST_VERSION=$(git tag | grep -E '[0-9]+.[0-9]+.[0-9]+.*' | sort -rV | head -1)
 NEW_VERSION=$(what-bump --from "$LATEST_VERSION" "$LATEST_VERSION")
 sed -i "" "s/0.0.0-UNRELEASED/$NEW_VERSION/g" Cargo.toml
