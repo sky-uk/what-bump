7210fec120eeaf56ff58d84f29006cfeb7a28717
colomboalb <alberto.colombo@skytv.it>
2020-01-20 12:16:42 +0100

    fix: release current branch, whatever it is

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -e
+git tag TEMP-RELEASE HEAD
 git checkout master
+git merge TEMP-RELEASE
 LATEST_VERSION=$(git tag | grep -E '[0-9]+.[0-9]+.[0-9]+.*' | sort -rV | head -1)
 NEW_VERSION=$(what-bump --from "$LATEST_VERSION" "$LATEST_VERSION")
 sed -i "" "s/0.0.0-UNRELEASED/$NEW_VERSION/g" Cargo.toml
@@ -9,4 +11,4 @@ git commit -a -m "chore: release version $NEW_VERSION"
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
 git merge "$NEW_VERSION"
 git push origin master "$NEW_VERSION"
-git branch -D temp-release
+git tag -d TEMP-RELEASE
