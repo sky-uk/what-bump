f57f6e030e72e72cc5ef91ed62106c286b4c7bb7
colomboalb <alberto.colombo@skytv.it>
2020-01-20 11:30:04 +0100

    fix: edit Cargo.toml in-place during release
    With no backup, coz we're already using git so we're not worried about corrupting it

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 set -e
 LATEST_VERSION=$(git tag | grep -E '[0-9]+.[0-9]+.[0-9]+.*' | sort -rV | head -1)
 NEW_VERSION=$(what-bump --from "$LATEST_VERSION" "$LATEST_VERSION")
-sed -i "s/0.0.0-UNRELEASED/$NEW_VERSION/g" Cargo.toml
+sed -i "" "s/0.0.0-UNRELEASED/$NEW_VERSION/g" Cargo.toml
 git commit -a -m "chore: release version $NEW_VERSION"
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
 git checkout master
