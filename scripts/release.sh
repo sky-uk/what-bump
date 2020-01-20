582760f89f538f4fd18275d2933918df5c25e79a
colomboalb <alberto.colombo@skytv.it>
2020-01-20 13:31:03 +0100

    build(release): simplify script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env sh
 set -e
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git tag TEMP-RELEASE HEAD
-git checkout master
-git merge TEMP-RELEASE -m "Merge $CURRENT_BRANCH into master"
 LATEST_VERSION=$(git tag | grep -E '[0-9]+.[0-9]+.[0-9]+.*' | sort -rV | head -1)
 NEW_VERSION=$(what-bump --from "$LATEST_VERSION" "$LATEST_VERSION")
 sed -i "" "s/0.0.0-UNRELEASED/$NEW_VERSION/g" Cargo.toml
 cargo check
 git commit -a -m "chore: release version $NEW_VERSION"
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
-git merge "$NEW_VERSION"
-git push origin master "$NEW_VERSION"
-git tag -d TEMP-RELEASE
+git checkout HEAD^ -- Cargo.toml Cargo.lock
+git commit -a -m "chore: restore UNRELEASED version"
+git push origin "$NEW_VERSION"
