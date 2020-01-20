6c24a2b7fec14ce92a476e3a6d214cae52551642
alberto <alberto.colombo@skytv.it>
2020-01-20 13:42:08 +0100

    build: Features/release script (#17)
    * fix: release current branch, whatever it is
    
    * build(release): improve script
    
    * build(release): simplify script
    
    * chore: release version 1.0.2
    
    * chore: restore UNRELEASED version

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
 set -e
-git checkout master
 LATEST_VERSION=$(git tag | grep -E '[0-9]+.[0-9]+.[0-9]+.*' | sort -rV | head -1)
 NEW_VERSION=$(what-bump --from "$LATEST_VERSION" "$LATEST_VERSION")
 sed -i "" "s/0.0.0-UNRELEASED/$NEW_VERSION/g" Cargo.toml
 cargo check
 git commit -a -m "chore: release version $NEW_VERSION"
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
-git merge "$NEW_VERSION"
-git push origin master "$NEW_VERSION"
-git branch -D temp-release
+git checkout HEAD^ -- Cargo.toml Cargo.lock
+git commit -a -m "chore: restore UNRELEASED version"
+git push origin "$NEW_VERSION"
