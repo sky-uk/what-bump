c04c71a7b248b2ccf7d2b2e1e08b1d73bbf8a356
colomboalb <alberto.colombo@skytv.it>
2020-01-20 12:11:45 +0100

    fix: perform release on master

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 set -e
-git checkout -b temp-release
+git checkout master
 LATEST_VERSION=$(git tag | grep -E '[0-9]+.[0-9]+.[0-9]+.*' | sort -rV | head -1)
 NEW_VERSION=$(what-bump --from "$LATEST_VERSION" "$LATEST_VERSION")
 sed -i "" "s/0.0.0-UNRELEASED/$NEW_VERSION/g" Cargo.toml
 cargo check
 git commit -a -m "chore: release version $NEW_VERSION"
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
-git checkout master
 git merge "$NEW_VERSION"
 git push origin master "$NEW_VERSION"
 git branch -D temp-release
