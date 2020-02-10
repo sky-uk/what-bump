4361dc4e88a089922be756f806e6a9b88c948207
colomboalb <alberto.colombo@skytv.it>
2020-02-10 15:10:39 +0100

    build: push immediately after release
    so CircleCI gets a chance to build it

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,6 +6,7 @@ sed -i "" "s/0.0.0-UNRELEASED/$NEW_VERSION/g" Cargo.toml
 cargo check
 git commit -a -m "chore: release version $NEW_VERSION"
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
+git push origin "$NEW_VERSION" develop
 git checkout HEAD^ -- Cargo.toml Cargo.lock
 git commit -a -m "chore: restore UNRELEASED version"
-git push origin "$NEW_VERSION"
+git push origin develop
