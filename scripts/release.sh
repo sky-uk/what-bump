483a9ee2603486ad072507b59bb2102c3383852a
colomboalb <alberto.colombo@skytv.it>
2020-02-28 16:30:08 +0100

    build: create git release and upload artifact
    Improved the release script to use ghr to perform the above actions.

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@ LATEST_VERSION=$(git tag | grep -E '[0-9]+.[0-9]+.[0-9]+.*' | sort -rV | head -1
 NEW_VERSION=$(what-bump --from "$LATEST_VERSION" "$LATEST_VERSION" --changelog CHANGELOG.md --overwrite)
 sed -i "" "s/0.0.0-UNRELEASED/$NEW_VERSION/g" Cargo.toml
 
-# 2. Commit and Tag
+# 2. Commit, Tag, and Create Release
 cargo check
 git commit -a -m "chore: release version $NEW_VERSION"
 git tag -a "$NEW_VERSION" -m "Release $NEW_VERSION"
@@ -16,10 +16,18 @@ git push origin "$NEW_VERSION" develop
 cargo publish
 
 #4. Publish to dockerhub
-docker build . -t albx79/what-bump:"$NEW_VERSION"
-docker push albx79/what-bump:"$NEW_VERSION"
+IMAGE_NAME=albx79/what-bump:"$NEW_VERSION"
+docker build . -t "$IMAGE_NAME"
+docker push "$IMAGE_NAME"
 
-#5. Restore snapshot version
+#5. Publish artifact to github
+id=$(docker create "$IMAGE_NAME")
+docker cp $id:/what-bump what-bump
+docker rm -v $id
+ghr $NEW_VERSION what-bump
+rm what-bump
+
+#6. Restore snapshot version
 git checkout HEAD^ -- Cargo.toml Cargo.lock
 git commit -a -m "chore: restore UNRELEASED version"
 git push origin develop
