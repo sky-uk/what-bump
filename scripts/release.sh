1ef6512f031c2069e5f3955c807ad178cb53077d
colomboalb <alberto.colombo@skytv.it>
2020-02-28 11:14:13 +0100

    build: use correct docker tag when releasing

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,7 +16,7 @@ git push origin "$NEW_VERSION" develop
 cargo publish
 
 #4. Publish to dockerhub
-docker build . -t what-bump:"$NEW_VERSION"
+docker build . -t albx79/what-bump:"$NEW_VERSION"
 docker push albx79/what-bump:"$NEW_VERSION"
 
 #5. Restore snapshot version
